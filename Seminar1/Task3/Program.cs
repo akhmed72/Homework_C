c2d5f18588272944526dbdda3b8a2db062523910
Akhmed <ahmed.shirinov72@gmail.com>
2022-12-13 19:54:53 +0500

    Task3

---
+++ Seminar1/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число > ");
+string inputValue = Console.ReadLine();
+int A = Convert.ToInt32(inputValue); 
+
+if(A%2==0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
