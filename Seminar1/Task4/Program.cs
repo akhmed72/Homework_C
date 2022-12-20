12184e5d2c8637ad8fbada11f0e41bb20dff646d
Akhmed <ahmed.shirinov72@gmail.com>
2022-12-20 13:10:24 +0500

    Task4

---
+++ Seminar1/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число N > ");
+string inputValue = Console.ReadLine();
+int N = Convert.ToInt32(inputValue);
+
+int unN = 1;
+
+while(unN <= N)
+{
+    if(unN%2 == 0)
+    Console.Write( unN + " ");
+    unN=unN+1;
+}
